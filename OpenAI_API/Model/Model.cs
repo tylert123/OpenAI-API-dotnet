c2762369d1385edb4645a367ca6b89d04cf0ac67
Tyler <torrest123@gmail.com>
2024-05-26 17:15:30 -0400

    Update "Model GPT4" and Model "GPT4_Vision" to use gpt-4o
    Update "Model GPT4" and Model "GPT4_Vision" to use gpt-4o. Left all others alone because of naming

--- OpenAI_API/Model/Model.cs
+++ OpenAI_API/Model/Model.cs
@@ -133,7 +133,7 @@ namespace OpenAI_API.Models
 		/// <summary>
 		/// More capable than any GPT-3.5 model, able to do more complex tasks, and optimized for chat. Will be updated with the latest model iteration.
 		/// </summary>
-		public static Model GPT4 => new Model("gpt-4") { OwnedBy = "openai" };
+		public static Model GPT4 => new Model("gpt-4o") { OwnedBy = "openai" };
 
 		/// <summary>
 		/// Same capabilities as the base gpt-4 model but with 4x the context length. Will be updated with the latest model iteration.
@@ -143,7 +143,7 @@ namespace OpenAI_API.Models
 		/// <summary>
 		/// Ability to understand images, in addition to all other GPT-4 Turbo capabilities. Returns a maximum of 4,096 output tokens. This is a preview model version and not suited yet for production traffic.
 		/// </summary>
-		public static Model GPT4_Vision => new Model("gpt-4-vision-preview") { OwnedBy = "openai" };
+		public static Model GPT4_Vision => new Model("gpt-4o") { OwnedBy = "openai" };
 
 		/// <summary>
 		///	The latest GPT-4 model with improved instruction following, JSON mode, reproducible outputs, parallel function calling, and more. Returns a maximum of 4,096 output tokens. This preview model is not yet suited for production traffic. 
